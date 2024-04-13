92032eed6110153be6d9f4b43cbda453d1557583
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-04-13 19:00:35 +0600

    Api work ongoing..

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,18 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+  } catch (error) {
+    console.error("Error while Register user. Try Again", error);
+    return {
+      success: false,
+      message: "Failed to Register User",
+    };
+  }
+}
